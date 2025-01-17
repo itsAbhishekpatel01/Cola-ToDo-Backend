194fb051fa7adb16024ab016c495c3b3fb4d9ede
Abhishek Patel <itsabhishekpatel01@gmail.com>
2025-01-17 12:25:39 +0530

    Changed email service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer')
 const sendEmail = async (email, subject, text)=>{
     try {
         const transporter = nodemailer.createTransport({
-            service: 'Gmail', // you can use any SMTP service like Gmail, Outlook
+            service: 'gmail', // you can use any SMTP service like Gmail, Outlook
             auth:{
                 user: "team.colatodo@gmail.com",
                 pass: `${process.env.APP_PASSWORD}`
@@ -11,7 +11,7 @@ const sendEmail = async (email, subject, text)=>{
         })
 
         const mailOptions = {
-            from: 'itsabhishekpatel01@gmail.com',
+            from: 'team.colatodo@gmail.com',
             to: email,
             subject: subject,
             html: text
